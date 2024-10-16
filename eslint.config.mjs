f67ff38c31c05e9f36508b3ffea66839a1840ebc
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-10-16 21:48:01 +0200

    change eslint config from browser to node

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
