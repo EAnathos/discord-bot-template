9681e44dd23628d7215b905eb711416b90db3fd4
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-10-17 12:35:45 +0200

    update jest config and add test

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'],
+};
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,12 +1,50 @@
+/**
+ * @file sample.test.ts
+ * @description Ce fichier contient les tests unitaires pour la fonction `sumFunction`
+ * située dans le module `sample.ts` du répertoire utils. Les tests vérifient si la
+ * fonction additionne correctement deux nombres.
+ */
+
 import { sumFunction } from '../src/utils/sample';
 
-describe('testing sumFunction', () => {
-  test('sumFunctionWithCorrectValues', () => {
-    const value1: number = 10;
-    const value2: number = 5;
+/**
+ * Test pour la fonction `sumFunction`
+ * @description Vérifie si la fonction `sumFunction` retourne la somme correcte de deux nombres.
+ */
+describe('sumFunction', () => {
+  // Test basique d'addition de deux nombres positifs
+  test('addition de deux nombres positifs', () => {
+    const result = sumFunction(2, 3);
+    expect(result).toBe(5); // Vérifie que 2 + 3 = 5
+  });
 
-    const result: number = 15;
+  // Test pour addition de deux nombres négatifs
+  test('addition de deux nombres négatifs', () => {
+    const result = sumFunction(-2, -3);
+    expect(result).toBe(-5); // Vérifie que -2 + -3 = -5
+  });
 
-    expect(sumFunction(value1, value2)).toBe(result);
+  // Test pour addition d'un nombre positif et d'un nombre négatif
+  test("addition d'un nombre positif et d'un nombre négatif", () => {
+    const result = sumFunction(5, -3);
+    expect(result).toBe(2); // Vérifie que 5 + (-3) = 2
+  });
+
+  // Test pour addition d'un nombre avec zéro
+  test("addition d'un nombre et zéro", () => {
+    const result = sumFunction(4, 0);
+    expect(result).toBe(4); // Vérifie que 4 + 0 = 4
+  });
+
+  // Test pour addition de deux zéros
+  test('addition de zéro avec zéro', () => {
+    const result = sumFunction(0, 0);
+    expect(result).toBe(0); // Vérifie que 0 + 0 = 0
+  });
+
+  // Test pour addition d'un grand nombre
+  test('addition de grands nombres', () => {
+    const result = sumFunction(1000000, 1000000);
+    expect(result).toBe(2000000); // Vérifie que 1000000 + 1000000 = 2000000
   });
 });
