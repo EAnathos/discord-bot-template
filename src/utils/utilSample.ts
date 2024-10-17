08343d1dee2466ac22688ecf4ceee4f106e80401
EAnathos <83123402+EAnathos@users.noreply.github.com>
2024-10-17 12:15:44 +0200

    add basic util file with proper structure

---
+++ src/utils/utilSample.ts
@@ -0,0 +1,15 @@
+/**
+ * This is a sample function that sums two numbers.
+ *
+ * @author EAnathos
+ * @version 1.0.0
+ *
+ * @example const sum = sumFunction(1, 2);
+ *
+ * @param {number} value1 - The first number to sum.
+ * @param {number} value2 - The second number to sum.
+ * @returns {number} - The sum of the two numbers.
+ */
+export function sumFunction(value1: number, value2: number): number {
+  return value1 + value2;
+}
